4d696f74bccb96f025c14fe0a92b9a272a532b8a
Ernest Poletaev <poletaev.ernest@gmail.com>
2019-02-02 16:03:03 +0700

    Separate branding as component

---
+++ src/components/brand.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Brand extends React.Component {
+  render() {
+    return (
+      <span>
+        <img className='brand-logo' src='/images/logo.png' />EP Showcase
+      </span>
+    )
+  }
+}
